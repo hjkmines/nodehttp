c62c6cd4c314cbf05e7bbdd8c4e73b995ff6b889
Tony Kim <hjkmines@gmail.com>
2020-08-06 21:52:34 -0600

    recieve data

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const http = require('http'); 
 
+const todos = [
+  {id: 1, text: 'todo 1'},
+  {id: 2, text: 'todo 2'},
+  {id: 3, text: 'todo 3'}
+]
+
 const server = http.createServer((req, res) => {
-  const {headers, url, method} = req;
-  console.log(headers, url, method) 
-  res.end(); 
+  res.statusCode = 404; 
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json', 
+    'X-Powered-By': 'Node.js'
+  })
+
+  let body = [];
+
+  req
+   .on('data', chunk => {
+    body.push(chunk); 
+  })
+   .on('end', () => {
+    body = Buffer.concat(body).toString(); 
+    console.log(body)
+  })
+
+  res.end(JSON.stringify({
+    sucess: true, 
+    data: todos
+  })); 
 })
 
 const PORT = 5000
